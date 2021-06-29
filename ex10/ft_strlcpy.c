3519b34722ed488b11739728fa7f4c404ab922ad
Nour-eddine Taleb <contact@noureddine.xyz>
2021-06-29 22:51:10 +0100

    fix bug on ex10

--- ex10/ft_strlcpy.c
+++ ex10/ft_strlcpy.c
@@ -6,14 +6,13 @@ unsigned int	ft_strlcpy(char *dest, char *src, unsigned int size)
 	len = 0;
 	i = 0;
 	while (src[len])
-	{
 		len++;
-	}
 	while (src[i] && i < (size - 1))
 	{
 		dest[i] = src[i];
 		i++;
 	}
-	dest[i] = 0;
+	if (size)
+		dest[i] = 0;
 	return (len);
 }

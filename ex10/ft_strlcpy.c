9138dba2e3141011a6cfae7546e0055655b2cf27
Noureddine Taleb <ntaleb@1337.ma>
2021-06-29 14:30:18 +0100

    ex10

--- ex10/ft_strlcpy.c
+++ ex10/ft_strlcpy.c
@@ -1,16 +1,19 @@
 unsigned int	ft_strlcpy(char *dest, char *src, unsigned int size)
 {
 	unsigned int	len;
+	unsigned int	i;
 
 	len = 0;
+	i = 0;
 	while (src[len])
 	{
 		len++;
 	}
-	while (*src && --size > 0)
+	while (src[i] && i < (size - 1))
 	{
-		*dest++ = *src++;
+		dest[i] = src[i];
+		i++;
 	}
-	*dest = 0;
+	dest[i] = 0;
 	return (len);
 }

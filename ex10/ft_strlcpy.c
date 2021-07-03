1c999b37a1c41108aaf03a11aa8f336cc63bbe3c
Noureddine Taleb <ntaleb@1337.ma>
2021-07-03 17:24:36 +0100

    finish strlcpy

--- ex10/ft_strlcpy.c
+++ ex10/ft_strlcpy.c
@@ -1,3 +1,26 @@
+unsigned int	ft_strlen(char *str)
+{
+	unsigned int	len;
+
+	len = 0;
+	while (*str++)
+		len++;
+	return (len);
+}
+
+char	*ft_memcpy(char *dst, char *src, unsigned int size)
+{
+	char	*dest;
+
+	dest = dst;
+	while (size)
+	{
+		*dst++ = *src++;
+		size--;
+	}
+	return (dest);
+}
+
 unsigned int	ft_strlcpy(char *dest, char *src, unsigned int size)
 {
 	unsigned int	srclen;
